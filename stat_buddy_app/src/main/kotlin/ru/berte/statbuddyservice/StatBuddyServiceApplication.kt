c24e50e8c7969b287d1b9f17cef6967e2b0814ab
Alex Omarov <shtil.a@yandex.ru>
2023-10-19 15:57:15 +0700

    SBS-1 Init service

--- stat_buddy_app/src/main/kotlin/ru/berte/statbuddyservice/StatBuddyServiceApplication.kt
+++ stat_buddy_app/src/main/kotlin/ru/berte/statbuddyservice/StatBuddyServiceApplication.kt
@@ -2,11 +2,14 @@ package ru.berte.statbuddyservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import reactor.core.publisher.Hooks
 
 @SpringBootApplication
 class StatBuddyServiceApplication
 
 fun main(args: Array<String>) {
+    // enable tracing propagation between different reactive pipelines
+    Hooks.enableAutomaticContextPropagation()
     @Suppress("SpreadOperator")
     runApplication<StatBuddyServiceApplication>(*args)
 }
--- stat_buddy_app/src/main/kotlin/ru/berte/statbuddyservice/infrastructure/config/ServiceProps.kt
+++ stat_buddy_app/src/main/kotlin/ru/berte/statbuddyservice/infrastructure/config/ServiceProps.kt
@@ -3,6 +3,8 @@ package ru.berte.statbuddyservice.infrastructure.config
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties
-data class ServiceProps(val contour: AppProps) {
+data class ServiceProps(val contour: AppProps, val logging: LoggingProps) {
     data class AppProps(val instance: String)
+    data class LoggingProps(val web: LoggingWebProps)
+    data class LoggingWebProps(val exclusions: List<String>)
 }
